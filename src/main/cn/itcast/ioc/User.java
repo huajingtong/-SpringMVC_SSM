5a5391f3f2a5a8968a835d6076bc776066586929
huajingtong <934395737@qq.com>
2018-07-03 16:44:06 +0800

    aaa哈哈哈

--- src/main/cn/itcast/ioc/User.java
+++ src/main/cn/itcast/ioc/User.java
@@ -5,6 +5,6 @@ package cn.itcast.ioc;
  */
 public class User {
     public void add() {
-        System.out.println("add......www...........");
+        System.out.println("add...hhh..........");
     }
 }
