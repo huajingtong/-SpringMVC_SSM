ff7a601126f922bb3385e571a6a6b785b6a77286
huajingtong <934395737@qq.com>
2018-07-03 21:32:56 +0800

    更新一下

---
+++ src/main/cn/itcast/property/TestIOC.java
@@ -0,0 +1,21 @@
+package cn.itcast.property;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by hjt on 2018/7/3.
+ */
+public class TestIOC {
+
+    @Test
+    public void demo1() {
+        // 1.加载Spring配置文件，把配置文件中的对象进行创建
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("bean1.xml");
+
+        Book book = (Book) context.getBean("book");
+        book.testBook();
+    }
+}
